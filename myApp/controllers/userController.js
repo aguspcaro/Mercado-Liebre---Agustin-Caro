256b18566ea2a7ce0921dba6de0ccbe5098ad0cd
aguspcaro <aguspcaro@gmail.com>
2020-12-23 23:22:15 -0300

    estilos

--- myApp/controllers/userController.js
+++ myApp/controllers/userController.js
@@ -2,7 +2,36 @@ let userController = {
 
     "login" : function (req, res) {
         res.render("login")
+    },
+
+
+
+    register: function(req, res) {
+
+        let user = {
+            email : req.body.email,
+            password : bcrypt.hashSync(rec.body.password, 10)
+        }
     }
 };
 
 module.exports = userController;
+
+/*
+
+    login : function(req, res) {
+
+        Storage.open('users.json')
+
+        let user = storage.findOnBy('email', req.body.email)
+
+        if (user) {
+            if(bcrypt.compareSync(req.body.password, user.password)) {
+                return res.send('bienvenido')
+
+            }
+        }
+        res.redirect('/users/login')
+
+    },
+*/
